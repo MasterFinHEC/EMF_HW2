d30ba5f9e2a6fe8f3ef892192e153c9d93938607
mimich94 <antoine.alexeev19@gmail.com>
2020-03-31 14:45:04 +0200

    Final codes

--- Plots.m
+++ Plots.m
@@ -1,4 +1,7 @@
 %% Plots
+
+%% 2.
+%Plot histogram of t-stats
 f = figure('visible','off');
 histfit(StStat_betas);
 hold on
@@ -14,29 +17,41 @@ xlabel('tStats')
 ylabel('Frequency')
 saveas(f,'Plots/HistogramBetas.png')
 
-%% CDF plots
-%Sim360
+%Plot histogram of the intercepts
+f = figure('visible','off');
+histfit(mus);
+hold on
+xline(meanmus,'color','k','Label','mean','LineWidth',2,'LabelOrientation','horizontal','LabelHorizontalAlignment','right','LabelVerticalAlignment','top');
+title('Histogram of intercepts')
+xlabel('Intercepts')
+ylabel('Frequency')
+saveas(f,'Plots/mus.png')
+
+% CDF plots
+%Computing the density points
 [a,y]=ecdf(tStat_betas);
 [g,z]=ecdf(tStat_Sim360);
+[w,q]=ecdf(tStat_Sim368);
+[o,l]=ecdf(tStat_Sim100);
+[s,u]=ecdf(tStat_Sim8);
 
+%Sim360
 f = figure('visible','off');
 plot(y,a)
 hold on
 plot(z,g)
+hold on
 title('Cumulative distribution function Comparative')
 xlabel('x')
 ylabel('F(x)')
 legend('null hypothesis','360 days & phi 0.96','location','southeast')
 saveas(f,'Plots/CumDistribution360.png')
 
-%% Sim368
-[a,y]=ecdf(tStat_betas);
-[g,z]=ecdf(tStat_Sim368);
-
+% Sim368
 f = figure('visible','off');
 plot(y,a)
 hold on
-plot(z,g)
+plot(q,w)
 title('Cumulative distribution function Comparative')
 xlabel('x')
 ylabel('F(x)')
@@ -44,13 +59,10 @@ legend('null hypothesis','360 days & phi 0.8','location','southeast')
 saveas(f,'Plots/CumDistribution368.png')
 
 %Sim368
-[a,y]=ecdf(tStat_betas);
-[g,z]=ecdf(tStat_Sim100);
-
 f = figure('visible','off');
 plot(y,a)
 hold on
-plot(z,g)
+plot(l,o)
 title('Cumulative distribution function Comparative')
 xlabel('x')
 ylabel('F(x)')
@@ -58,54 +70,65 @@ legend('null hypothesis','100 days & phi 0.96','location','southeast')
 saveas(f,'Plots/CumDistribution100.png')
 
 %Sim8
-
-[a,y]=ecdf(tStat_betas);
-[g,z]=ecdf(tStat_Sim8);
-
 f = figure('visible','off');
 plot(y,a)
 hold on
-plot(z,g)
+plot(u,s)
 title('Cumulative distribution function Comparative')
 xlabel('x')
 ylabel('F(x)')
 legend('null hypothesis','100 days & phi 0.8','location','southeast')
 saveas(f,'Plots/CumDistribution8.png')
 
-%Plot all together
-
-[a,y]=ecdf(tStat_betas);
-[g,z]=ecdf(tStat_Sim360);
-[w,u]=ecdf(tStat_Sim368);
-[o,q]=ecdf(tStat_Sim100);
-[v,m]=ecdf(tStat_Sim8);
+% Histograms of the t-stats
+f = figure('visible','off');
+histogram(tStat_Sim360);
+hold on
+histogram(StStat_betas);
+title('Histogram of t-stats comparative')
+xlabel('t-Stats')
+ylabel('Frequency')
+legend('null hypothesis','360 days & phi 0.96','location','southeast')
+saveas(f,'Plots/HistSim360.png')
 
 f = figure('visible','off');
-plot(y,a)
+histogram(tStat_Sim368);
 hold on
-plot(z,g)
+histogram(StStat_betas);
+title('Histogram of t-stats comparative')
+xlabel('t-Stats')
+ylabel('Frequency')
+legend('null hypothesis','368 days & phi 0.8','location','southeast')
+saveas(f,'Plots/HistSim368.png')
+
+f = figure('visible','off');
+histogram(tStat_Sim100);
 hold on
-plot(u,w)
+histogram(StStat_betas);
+title('Histogram of t-stats comparative')
+xlabel('t-Stats')
+ylabel('Frequency')
+legend('null hypothesis','100 days & phi 0.96','location','southeast')
+saveas(f,'Plots/HistSim100.png')
+
+f = figure('visible','off');
+histogram(tStat_Sim8);
 hold on
-plot(q,o)
-hold on
-plot(m,v)
-title('Cumulative distribution function Comparative all together')
-xlabel('x')
-ylabel('F(x)')
-legend('null hypothesis','360 days & phi 0.96','360 days & phi 0.8','100 days & phi 0.96','100 days & phi 0.8','Location','southeast','FontSize',6)
-saveas(f,'Plots/CumDistributionall.png')
+histogram(StStat_betas);
+title('Histogram of t-stats comparative')
+xlabel('t-Stats')
+ylabel('Frequency')
+legend('null hypothesis','100 days & phi 0.8','location','southeast')
+saveas(f,'Plots/HistSim8.png')
 
 %% 3
-
 % Histogram of tStat of Residuals
-
 f = figure('visible','off');
 histfit(StStat_R);
 hold on
 xline(CriticalValueR_1,'color','r','Label','Critical Value 1%','LineWidth',2,'LabelHorizontalAlignment','left','LabelVerticalAlignment','top');
 hold on
-xline(meantStatR,'color','k','Label','mean','LineWidth',2,'LabelOrientation','horizontal','LabelHorizontalAlignment','right','LabelVerticalAlignment','top');
+xline(meantStat_R,'color','k','Label','mean','LineWidth',2,'LabelOrientation','horizontal','LabelHorizontalAlignment','right','LabelVerticalAlignment','top');
 hold on
 xline(CriticalValueR_5,'color','r','Label','Critical Value 5%','LineWidth',2,'LabelHorizontalAlignment','left','LabelVerticalAlignment','top');
 hold on
@@ -114,3 +137,34 @@ title('Histogram tStats Residuals')
 xlabel('tStats')
 ylabel('Frequency')
 saveas(f,'Plots/HistogramR.png')
+
+%Histogram of intercepts
+f = figure('visible','off');
+histfit(musR);
+hold on
+xline(meanmusR,'color','k','Label','mean','LineWidth',2,'LabelOrientation','horizontal','LabelHorizontalAlignment','right','LabelVerticalAlignment','top');
+title('Histogram of intercepts')
+xlabel('Intercepts')
+ylabel('Frequency')
+saveas(f,'Plots/musR.png')
+
+%DDM
+f = figure('visible','off');
+plot(date,logUS)
+hold on
+plot(date,DDMUS)
+title('DDM US')
+xlabel('Date')
+ylabel('log Prices')
+legend('US log Prices','DDM US','location','southeast')
+saveas(f,'Plots/DDMUS.png')
+
+f = figure('visible','off');
+plot(date,logUK)
+hold on
+plot(date,DDMUK)
+title('DDM UK')
+xlabel('Date')
+ylabel('log Prices')
+legend('UK log Prices','DDM UK','location','southeast')
+saveas(f,'Plots/DDMUK.png')
--- tabletolatex2.m
+++ tabletolatex2.m
@@ -6,13 +6,14 @@
 % Due Date: 5 March 2020
 %==========================================================================
 %% Creating the latex code 
-addpath(genpath('C:\Users\41797\Desktop\UNIL-Master\EMF\Homework2\'))
+addpath(genpath('C:\Users\41797\Documents\GitHub\EMF_HW2\'))
 
 table2latex(CriticalValues,'LatexTable\CriticalValues.tex')
 table2latex(Power_of_test,'LatexTable\Power_of_test.tex')
 table2latex(tStat_data,'LatexTable\tStat_data.tex')
 table2latex(CriticalValuesR,'LatexTable\CriticalValuesR.tex')
 table2latex(Coefficients,'LatexTable\Coefficients.tex')
-%%
 table2latex(tStatR,'LatexTable\tStatR.tex')
 table2latex(stat3b,'LatexTable\stat3b.tex')
+table2latex(TCriticalValues,'LatexTable\TestCriticalValues.tex')
+table2latex(TCriticalValuesR,'LatexTable\TestCriticalValuesR.tex')
